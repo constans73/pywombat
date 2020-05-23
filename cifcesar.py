16d9a86cd76811c57315e4840baa075236d1cb04
constans <constans73@gmail.com>
2020-05-23 16:45:50 +0200

    se ha corregido el ejercicio antes daba algun error debido a rsecuencia.replace ya que cambia caracteres ya cambiados se ha corregido quitando los for y poniendo un while e ir cambiando caracter e implementando la frase caracter por caracter.

--- cifcesar.py
+++ cifcesar.py
@@ -2,23 +2,29 @@ def cifrado(sentencia, llave=10):
 	llave = int(llave)
 	sentencia = sentencia.lower()
 	letras = "abcdefghijklmnñopqrstuvwxyz"   #longitud 26
+
+		
+	numero = 0
+	frase_modificada =""
+	while numero != len(sentencia):	
 	
-	for i in sentencia:
-
-		if i in letras:
-
-			salto = letras.index(i) + llave
-				
+		if sentencia[numero] in letras:
+		
+			salto = letras.index(sentencia[numero]) + llave				#se elige el salto que tiene que dar indice de
+																	#letras mas el salto por el usuario
 			if salto > 26:
-				salto =  salto - 27					#si rompe la longuitud de letras
-
-				sentencia = sentencia.replace (i,letras[salto])
+				salto = salto - 27										#si el salto excede el limite de 26
+				frase_modificada = frase_modificada + letras[salto]
 			else:
-				sentencia = sentencia.replace (i,letras[salto])
+				frase_modificada = frase_modificada + letras[salto]
+		
+		else:
 
+			frase_modificada = frase_modificada + sentencia[numero]		#si nes caracter de letras se queda igual
 
-	return sentencia
+		numero += 1
 
+	return frase_modificada
 
 
 texto = input ("Introduce el texto a codificar\n")
